6433133155c4a44df15413144b74619a99a678cc
Mohamed Akrem Chabchoub <121046693+mohamedch7@users.noreply.github.com>
2024-08-26 17:53:31 +0100

    Enhance the script

--- kubectl-guard.sh
+++ kubectl-guard.sh
@@ -6,21 +6,18 @@ CURRENT_CONTEXT=$(kubectl config current-context)
 # List of potentially dangerous commands
 DANGEROUS_COMMANDS=("apply" "delete")
 
-# Check if the current command is one of the dangerous commands
-for cmd in "${DANGEROUS_COMMANDS[@]}"; do
+# Identifier for production clusters
+PROD_IDENTIFIER="prod"
 
-# We need always to use the word "prod" in the prod-cluster's name
-# $1: the first argument passed to the script
-if [[ "$1" == "$cmd" && "$CURRENT_CONTEXT" == *prod* ]]; then
-        echo -e "\033[0;31mWarning! You are about to execute '$cmd' on the '$CURRENT_CONTEXT' cluster.\033[0m"
-        read -p "Are you sure you want to continue? [y/N]: " -r
-        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-            echo "Operation aborted!"
-            exit 1
-        fi
+# Check if the current command is dangerous and the context is a production cluster
+if [[ " ${DANGEROUS_COMMANDS[*]} " =~ " $1 " && "$CURRENT_CONTEXT" == *"$PROD_IDENTIFIER"* ]]; then
+    echo -e "\033[0;31mWarning! You are about to execute '$1' on the '$CURRENT_CONTEXT' cluster.\033[0m"
+    read -p "Are you sure you want to continue? [y/N]: " -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "Operation aborted!"
+        exit 1
     fi
-done
+fi
 
 # Execute the original kubectl command
-# $@: refers to all arguments passed to the script
 kubectl "$@"
